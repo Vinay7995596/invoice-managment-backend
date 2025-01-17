4aa0a4305fdc4869c280ba44d02c40289f3f4d62
Vinay7995596 <ragichattuvinay7995@gmail.com>
2025-01-17 20:14:13 +0530

    first commit

--- index.js
+++ index.js
@@ -5,11 +5,14 @@ const cors = require('cors')
 const port = 5500;
 
 const url = 'mongodb://localhost:27017/test'
+const anotherUrl = 'mongodb+srv://ragichattuvinay7995:<sIptkomxoE6hoZJn>@cluster0.fzael.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+
+const deployUrl = 'mongodb+srv://ragichattuvinay7995:sIptkomxoE6hoZJn@cluster0.fzael.mongodb.net/loginDetails'
 
 app.use(express.json())
 app.use(cors())
 
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true,})
+mongoose.connect(deployUrl, { useNewUrlParser: true, useUnifiedTopology: true,})
 .then(() => {
     console.log('success to connect DB')
 })
@@ -25,7 +28,7 @@ const schema = new mongoose.Schema({
 
 
 
-const moongoseModel = mongoose.model('logindetails', schema)
+const moongoseModel = mongoose.model('loginUsers', schema)
 
 
 app.post('/signup', async (req, res) => {
